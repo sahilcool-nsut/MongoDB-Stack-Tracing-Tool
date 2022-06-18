6dfec4aa7c9205055b3ccdfffb11bf554ee96b44
Sahil Chawla <schawla516gmail.com>
2022-06-18 17:20:34 +0530

    Created Function Counts function

--- newerMain.sh
+++ newerMain.sh
@@ -25,7 +25,7 @@ NL=$'\n'                #Newline character used for some parsing purposes
 # --------------Function Definitions--------------
 multiThreadDetail(){
     index=$1
-    local fileName="json$index.json"
+    local fileName="OutputFiles/json$index.json"
     local JSON_STRING=$( jq -n \
                   '{threads:{}}')
     echo "$JSON_STRING" > $fileName
@@ -58,6 +58,7 @@ multiThreadDetail(){
     # Have to sort by 1st field (threadId) so that order of taking stack remains consistent in all threads. 
     done < <(top -H -bn1 | grep -m $TOP_N_THREADS "conn" | sort -n -k1) 
 }
+
 mergeJson(){
 
     jq -s 'def deepmerge(a;b):
@@ -71,13 +72,56 @@ mergeJson(){
             $val
         end)
         );
-    deepmerge({}; .)' json*.json > "merged.json"
+    deepmerge({}; .)' OutputFiles/json*.json > "OutputFiles/merged.json"
 
 }
+
+getFunctionCounts(){
+
+    while read -r line;do
+        currStack=$(echo -e "$line")
+        # echo "$currStack"
+        while read -r line; do
+            
+            # echo "$line"
+            # For lines #TID: where no output will be produced in this while loop
+            if [[ "$line" = "" ]];then
+
+                continue
+            fi
+            if [[ ${functionCallCounts[$line]} ]]; then
+                
+                local count=${functionCallCounts[$line]}
+                let count=$count+1
+                functionCallCounts[$line]=$count
+            else
+                functionCallCounts[$line]=1
+            fi
+        done< <(echo "$currStack" | awk '{$1=$2=""; print $0}')
+
+    done< <(jq --raw-output '.threads[] | .iterations[] | .threadStack' OutputFiles/merged.json)
+    
+    # Basically first echo the COUNT first, and then the FUNCTION, so that if FUNCTION has spaces, they occur later
+    # And we can sort by first field
+    # -r = sorting in reverse
+    # -n = numeric sorting
+    # By default takes first field, else have to specify as -k2 for 2nd etc.
+    currTime=$(($(date +%s%N)/1000000))
+    echo "Sorting Started": $currTime <&2
+    for function in "${!functionCallCounts[@]}"; do 
+        echo "${functionCallCounts["$function"]} ${function}"
+    done | sort -rn | while read number function; do 
+        echo "${function} - ${number}" >> $OUTPUT_FILE_NAME
+        echo "" >> $OUTPUT_FILE_NAME
+    done
+
+}
+
 # --------------Program Flow--------------
 # Create Output File
 echo > $OUTPUT_FILE_NAME
 
+# Going in!!
 # Get PID of mongod
 pid=$(pidof mongod)
 echo "Process ID of mongod is: $pid" >> $OUTPUT_FILE_NAME
@@ -88,11 +132,75 @@ for ((i=0;i<NUMCALLS;i++)); do
     sleep $INTERVAL
 done
 
+# sequentialThreadDetail
+
 wait 
 
 echo "Merging"
 mergeJson
 echo "Merged"
 
+
+currTime=$(($(date +%s%N)/1000000))
+echo "Function Call Starting Time: $currTime"
+declare -A functionCallCounts
+getFunctionCounts
+
+
+
 currTime=$(($(date +%s%N)/1000000))
 echo "Ending time: $currTime"
+
+
+
+
+
+# OTHER TESTING CODE
+# sequentialThreadDetail(){
+#     local fileName="jsonSequential$index.json"
+#     local JSON_STRING=$( jq -n \
+#                   '{threads:{}}')
+#     echo "$JSON_STRING" > $fileName
+#     while read -r detail;
+#     do
+#         for ((i=0;i<NUMCALLS;i++)); do
+#             # echo "$detail"
+
+#             local resultsArray=($(echo $detail | awk '{ print $1, $8, $9, $12 }'))
+#             local threadId=${resultsArray[0]}
+#             local threadState=${resultsArray[1]}
+#             local threadCPU=${resultsArray[2]}
+#             if (( $(echo "$threadCPU < $CPU_THRESHOLD" |bc -l) )); then
+#                 break;
+#             fi
+#             # echo $threadId
+#             local threadName=${resultsArray[3]}
+#             local stack="$(sudo eu-stack -1 -p $threadId)"
+            
+#             local currTime=$(($(date +%s%N)/1000000))
+#             # echo $currTime
+#             local JSON_TEMP=$(jq \
+#             --arg threadId "$threadId:$i" \
+#             --arg threadName "$threadName" \
+#             --arg threadState "$threadState" \
+#             --arg threadCPU "$threadCPU" \
+#             --arg threadStack "$stack" \
+#             --arg currTime "$currTime" \
+#             --arg iteration "$i" \
+#             '.threads += {($threadId): {threadId: $threadId, iterations: [{iteration: $iteration, timeStamp: $currTime, threadName: $threadName, threadState: $threadState, threadCPU: $threadCPU, threadStack: $threadStack}] }}' $fileName)
+#             echo "$JSON_TEMP" > $fileName
+            
+#             # Handling Error in time
+#             local currTime2=$(($(date +%s%N)/1000000))
+#             local diff=$(bc <<< "scale=2; ($currTime2-$currTime)/1000")
+#             local newInterval=$(bc <<< "scale=2; ($INTERVAL-$diff)")
+#             if (( $(echo "$newInterval < 0" |bc -l) )); then
+#                 newInterval=0
+#             fi
+#         #Error handling complete
+            
+#             sleep $newInterval
+#         done
+        
+#     done < <(top -H -bn1 | grep -m $TOP_N_THREADS "conn") 
+# }
--- trial.sh
+++ trial.sh
@@ -30,37 +30,37 @@
 
 currTime=$(($(date +%s%N)/1000000))
 echo $currTime
-totalCalls=$(ps -T -p 8188 | grep "conn" -c)
+# totalCalls=$(ps -T -p 8188 | grep "conn" -c)
 
-multiThreadDetail(){
-    index=$1
-    local JSON_STRING=$( jq -n \
-                  '{threads:[]}')
-    echo "$JSON_STRING" > "json$index.json"
-    while read -r detail;
-    do
-        resultsArray=($(echo $detail | awk '{ print $1, $8, $9, $12 }'))
-        threadId=${resultsArray[0]}
-        threadState=${resultsArray[1]}
-        threadCPU=${resultsArray[2]}
-        # echo 
-        # if (( $(echo "$threadCPU < 20" |bc -l) )); then
-        #     break;
-        # fi
-        # echo $threadId
-        threadName=${resultsArray[3]}
-        stack="$(sudo eu-stack -1 -p $threadId)"
-                  # Create empty threads arary which will be filled later
-        local fileName="OutputFiles/IndividualOutput_$currTime.json"            
+# multiThreadDetail(){
+#     index=$1
+#     local JSON_STRING=$( jq -n \
+#                   '{threads:[]}')
+#     echo "$JSON_STRING" > "json$index.json"
+#     while read -r detail;
+#     do
+#         resultsArray=($(echo $detail | awk '{ print $1, $8, $9, $12 }'))
+#         threadId=${resultsArray[0]}
+#         threadState=${resultsArray[1]}
+#         threadCPU=${resultsArray[2]}
+#         # echo 
+#         # if (( $(echo "$threadCPU < 20" |bc -l) )); then
+#         #     break;
+#         # fi
+#         # echo $threadId
+#         threadName=${resultsArray[3]}
+#         stack="$(sudo eu-stack -1 -p $threadId)"
+#                   # Create empty threads arary which will be filled later
+#         local fileName="OutputFiles/IndividualOutput_$currTime.json"            
         
-        local JSON_TEMP=$(jq \
-        --arg threadId "$threadId" \
-        --arg threadName "$threadName" \
-        --arg threadState "$threadState" \
-        --arg threadCPU "$threadCPU" \
-        --arg threadStack "$stack" \
-        '.threads += [{threadId: $threadId, threadName: $threadName, threadState: $threadState, threadCPU: $threadCPU, threadStack: $threadStack}]' "json$index.json")
-        echo "$JSON_TEMP" > "json$index.json"
+#         local JSON_TEMP=$(jq \
+#         --arg threadId "$threadId" \
+#         --arg threadName "$threadName" \
+#         --arg threadState "$threadState" \
+#         --arg threadCPU "$threadCPU" \
+#         --arg threadStack "$stack" \
+#         '.threads += [{threadId: $threadId, threadName: $threadName, threadState: $threadState, threadCPU: $threadCPU, threadStack: $threadStack}]' "json$index.json")
+#         echo "$JSON_TEMP" > "json$index.json"
         # echo "$stack"
         # threadId=$(echo $detail | awk '{print $1}')
         
@@ -77,15 +77,98 @@ multiThreadDetail(){
         # echo $threadId
         # currTime=$(($(date +%s%N)/1000000))
         # echo $currTime
-        # printf "\n"
-    done < <(top -H -bn1 | grep -m 10 "conn")
-}
-for ((i=0;i<3;i++)); do
-    multiThreadDetail $i &
-    sleep 0.2
-done
+#         # printf "\n"
+#     done < <(top -H -bn1 | grep -m 10 "conn")
+# }
+# for ((i=0;i<3;i++)); do
+#     multiThreadDetail $i &
+#     sleep 0.2
+# done
 
-wait
+# wait
+
+# # 1655438447582
+
+readJsonData(){
+
+    declare -A functionCallCounts
+
+    while read -r line;do
+        currStack=$(echo -e "$line")
+        # echo "$currStack"
+        while read -r line; do
+            
+            # echo "$line"
+            # For lines #TID: where no output will be produced in this while loop
+            if [[ "$line" = "" ]];then
+
+                continue
+            fi
+            if [[ ${functionCallCounts[$line]} ]]; then
+                
+                local count=${functionCallCounts[$line]}
+                let count=$count+1
+                functionCallCounts[$line]=$count
+            else
+                functionCallCounts[$line]=1
+            fi
+        done< <(echo "$currStack" | awk '{$1=$2=""; print $0}')
+
+    done< <(jq --raw-output '.threads[] | .iterations[] | .threadStack' OutputFiles/merged.json)
+    
+    # Basically first echo the COUNT first, and then the FUNCTION, so that if FUNCTION has spaces, they occur later
+    # And we can sort by first field
+    # -r = sorting in reverse
+    # -n = numeric sorting
+    # By default takes first field, else have to specify as -k2 for 2nd etc.
+    for function in "${!functionCallCounts[@]}"; do 
+        echo "${functionCallCounts["$function"]} ${function}"
+    done | sort -rn | while read number function; do 
+        echo "${function} - ${number}"
+    done
+
+}
+
+readJsonData
 currTime=$(($(date +%s%N)/1000000))
 echo $currTime
-# 1655438447582
+
+
+
+
+# String Interpolation is independent of the dynamic threadIDs by which my objects start
+    # Was necessary to start objects by dynamic ID as they had to be merged.
+    # | is used as seperator instead of spaces.
+    # didnt use raw-output as it converted escape characters and didnt allow to loop easily
+    # as didnt use raw-output, was getting one double quote in start and one in end, so ignored them by storing in garbage variable
+    # Currently, threadStack has \n characters, so can be printed using -e 
+# CREATED PROBLEMS WITH MORE THAN 2 FIELDS, FOR EX CREATED 64 ENTRIES IF I DID 6 fields (2^6 iguess)
+# while IFS='|' read -r doubleQuote threadId iteration threadName threadState threadCPU threadStack doubleQuote; do
+#         echo "ThreadID: $threadId"
+#         echo "Iteration: $iteration"
+#         echo "Thread Name: $threadName"
+#         echo "Thread State: $threadState"
+#         echo "Thread CPU: $threadCPU"
+#         # echo "Thread Stack: $threadStack" 
+#         threadStackWithoutEscape=$(echo -e "$threadStack")  # unescapes the string
+#         # echo "$threadStackWithoutEscape" | awk '{$1=$2=""; print $0}'
+#         # while read -r line; do
+            
+#         #     echo "$line"
+#         #     # For lines #TID: where no output will be produced in this while loop
+#         #     if [[ "$line" = "" ]];then
+
+#         #         continue
+#         #     fi
+#         #     if [[ ${functionCallCounts[$line]} ]]; then
+                
+#         #         local count=${functionCallCounts[$line]}
+#         #         echo $count
+#         #         let count=$count+1
+#         #         functionCallCounts[$line]=$count
+#         #     else
+#         #         functionCallCounts[$line]=1
+#         #     fi
+#         # done< <(echo "$threadStackWithoutEscape" | awk '{$1=$2=""; print $0}')
+        
+#     done< <(jq '.threads[] | "|\(.threadId)|\(.iterations[].iteration)|\(.iterations[].threadName)|\(.iterations[].threadState)|\(.iterations[].threadCPU)|\(.iterations[].threadStack)|"' OutputFiles/merged.json)
