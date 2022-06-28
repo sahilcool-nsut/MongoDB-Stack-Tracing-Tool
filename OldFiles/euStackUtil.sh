c6ea08d6ed9c24e9fb0d85f5cdf611cb20c35428
Sahil Chawla <schawla516gmail.com>
2022-06-28 07:30:39 +0530

    removed old files

--- OldFiles/euStackUtil.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-timeStamp=$1
-pid=$2
-
-echo "Taking Stack Trace with timestamp: ${timeStamp} and pid: ${pid}"
-
-stackDump="$(sudo eu-stack -p $pid --source))"
-echo "$stackDump" > "stackDumps/FullStack_${timeStamp}"
-
